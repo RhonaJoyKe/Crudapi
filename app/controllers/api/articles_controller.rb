6cc5c3c991e1b10793f4c15735704041f4860883
RhonaJoyKe <rhonajoy8@gmail.com>
2022-12-20 13:27:01 -0500

    creates the show update

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,11 +1,30 @@
 class Api:: ArticlesController < ApplicationController
   def index
+    articles=Article.all
+    render json: articles, status:200
   end
 
   def show
+    article= Article.find_by(id: params[:id])
+    if article
+      render json: article, status: 200
+    else
+      render json:{
+        error:"Article not found"
+      }
+    end
   end
 
   def update
+    article= Article.find_by(id: params[:id])
+    if article
+      article.update(title: params[:title],body: params[:body],author: params[:author])
+      render json: "Article was updated successfully"
+    else
+      render json: {
+        error: "Article Not Found"
+      }
+    end
   end
 
   def destroy
