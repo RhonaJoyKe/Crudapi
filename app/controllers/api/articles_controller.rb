b8ac1e1bc7a80cbe63da575db1bf8b1e906e71be
RhonaJoyKe <rhonajoy8@gmail.com>
2022-12-20 13:03:58 -0500

    create function

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,4 +1,4 @@
-class Api::ArticlesController < ApplicationController
+class Api:: ArticlesController < ApplicationController
   def index
   end
 
@@ -10,4 +10,22 @@ class Api::ArticlesController < ApplicationController
 
   def destroy
   end
-end
+  def create
+    article=Article.new(
+      title:article_params[:title],
+      body: article_params[:body],
+      author: article_params[:author]
+    )
+    if article.save
+          render json: article,status: 200
+    else
+      render json: {
+        error: "Error creating"
+      }
+    end
+  end
+  private
+    def article_params
+      params.require(:article).permit( [:title, :body,:author])
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
-      resources: articles,only:[:index, :show, :create, :update, :destroy]
+      resources :articles,only:[:index, :show, :create, :update, :destroy]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
